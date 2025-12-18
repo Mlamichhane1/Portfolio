b3940b15d7a2f1fbd38bb43e8b393e2d9bb4f060
Madhav Lamichhane <148424282+Mlamichhane1@users.noreply.github.com>
2025-12-18 10:50:36 -0500

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Playfair Display", "ui-serif", "Georgia"],
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+};
